10e6c7eab19f23cc58b52edd5913289c2c579abb
Clark Mulvey <clark.mulvey@churchofjesuschrist.org>
2022-09-03 14:34:36 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,176 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./calculator.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Square extends React.Component {
+  render() {
+    return <button className="square">{/* TODO */}</button>;
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Chart extends React.Component {
+  render() {
+    return (
+      <div className="calculator">
+        <h1>Weight & Balance Form</h1>
+        <table className="calcTable">
+          <tr>
+            <th></th>
+            <th>Weight</th>
+            <th>Arm</th>
+            <th>Moment</th>
+          </tr>
+          <tr>
+            <th>Aircraft</th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+          </tr>
+          <tr>
+            <th>Pilot & Front Pass</th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+          </tr>
+          <tr>
+            <th>Mid Passengers</th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+          </tr>
+          <tr>
+            <th>Rear Passengers</th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+          </tr>
+          <tr>
+            <th>Aft Bag #1</th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+          </tr>
+          <tr>
+            <th>Aft Bag #2</th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+          </tr>
+          <tr>
+            <th>Fuel</th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+          </tr>
+          <tr>
+            <th>Total</th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+            <th>
+              <input></input>
+            </th>
+          </tr>
+        </table>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Chart />);
