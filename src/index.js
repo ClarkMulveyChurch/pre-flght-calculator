0cb87f54ed9e9465311c75bb3cf48859f9cc9af4
Clark Mulvey <clark.mulvey@churchofjesuschrist.org>
2022-09-06 04:53:18 -0600

    added service worker for PWA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import "./index.css";
 import "./calculator.css";
 import app from "./utils/database/config"
 import dbActions from "./utils/database/dbActions"
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 class Square extends React.Component {
   render() {
